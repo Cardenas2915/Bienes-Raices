7636975e03fd5116e4be7cf1609fb0fd05377de9
Cardenas2915 <eduar.cardenas15@gmail.com>
2023-12-21 16:07:57 -0500

    se crearon las vistas para Login,registro y olvide-password.su respectivos formularios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,18 +1,26 @@
 const formularioLogin = (req, res) =>{
     //es la ruta de la vista que queremos mostrar
     res.render('auth/login', {
-        
+        pagina: 'Iniciar sesion'
     }) 
 }
 
 const formularioRegistro = (req, res) =>{
     //es la ruta de la vista que queremos mostrar
     res.render('auth/registro', {
-        
+        pagina: 'Crear cuenta'
+    }) 
+}
+
+const formularioOlvidePassword = (req, res) =>{
+    //es la ruta de la vista que queremos mostrar
+    res.render('auth/olvide-Password', {
+        pagina: 'Recupera tu acceso'
     }) 
 }
 
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { formularioLogin, formularioRegistro } from '../controllers/usuarioController.js'
+import { formularioLogin, formularioRegistro, formularioOlvidePassword } from '../controllers/usuarioController.js'
 
 //Crear la app
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 //Routing
 router.get('/login', formularioLogin) //esta funcion viene definida en el controlador que es donde se esta retornando la vista
 router.get('/registro', formularioRegistro)
+router.get('/olvide-Password', formularioOlvidePassword)
 
 
 export default router
